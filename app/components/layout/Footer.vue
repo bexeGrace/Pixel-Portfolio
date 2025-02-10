30afe13a3d3824b78d13434de3f11b789d72a2ad
Betsegaw Mesele <146959506+bexeGrace@users.noreply.github.com>
2025-02-10 14:31:49 +0300

    try to fix issues with packages

--- app/components/layout/Footer.vue
+++ app/components/layout/Footer.vue
@@ -3,7 +3,7 @@
     <Divider />
     <footer class="mx-auto flex max-w-7xl flex-col items-center gap-4 px-4 py-6">
       <Logo :size="8" />
-      <span class="text-center text-sm text-white/60">
+      <span class="text-center text-sm dark:text-white/60">
         © {{ new Date().getFullYear() }}, <ULink to="">Pixel</ULink> - {{ $t("global.all_rights_reserved") }}.
       </span>
     </footer>
